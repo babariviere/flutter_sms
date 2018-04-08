e8ad61cf266c93adb16d290eb81addfd44d50157
Joan Pablo <johnpjm85@gmail.com>
2018-04-08 13:02:56 -0400

    Some colors to the conversations and contact's avatar

--- example/lib/utils/group.dart
+++ example/lib/utils/group.dart
@@ -2,29 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:sms/sms.dart';
 
 class MessageGroupService {
-
-  final BuildContext context;
-
-  static MessageGroupService of(BuildContext context){
+  static MessageGroupService of(BuildContext context) {
     return new MessageGroupService._private(context);
   }
 
   MessageGroupService._private(this.context);
 
+  final BuildContext context;
+
   List<Group> groupByDate(List<SmsMessage> messages) {
     final groups = new _GroupCollection();
-    messages.forEach((message){
-      String groupLabel = MaterialLocalizations.of(context).formatFullDate(message.date);
-      if (groups.contains(groupLabel)){
+    messages.forEach((message) {
+      String groupLabel =
+          MaterialLocalizations.of(context).formatFullDate(message.date);
+      if (groups.contains(groupLabel)) {
         groups.get(groupLabel).addMessage(message);
-      }
-      else {
-        groups.add(
-            new Group(
-                groupLabel,
-                messages: [message]
-            )
-        );
+      } else {
+        groups.add(new Group(groupLabel, messages: [message]));
       }
     });
     return groups.groups;
